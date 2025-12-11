d31c1d9178da125077496769f1fe29f4caa92ef1
Junior Ferreira <jr.ferreira0018@gmail.com>
2025-12-11 10:41:30 -0300

    corrigindo bugs de deploy

---
+++ app/adm/dashboard/page.jsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+    return (
+        <div className="text-white mt-16">
+            <h1 className="text-4xl font-bold mb-6">Dashboard Administrativo</h1>
+            <p className="text-lg">Bem-vindo ao painel administrativo do Reduto!</p>
+        </div>
+    );
+}
